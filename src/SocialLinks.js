b92536c51bd6326d1195e7575ae929272e1708f4
MeghanaKatraju31 <159526348+MeghanaKatraju31@users.noreply.github.com>
2025-01-28 18:41:28 -0600

    Update SocialLinks.js

--- src/SocialLinks.js
+++ src/SocialLinks.js
@@ -9,8 +9,8 @@ const SocialLinks = () => {
       <a href="https://www.linkedin.com/in/meghana-k-568934211/" target="_blank" rel="noopener noreferrer">
         <img src="/icons/linkedin-icon.svg" alt="LinkedIn" />
       </a>
-      <a href="/Resume/MyResume.pdf" target="_blank" rel="noopener noreferrer">
-        <img src="/icons/resume-icon.svg" alt="Resume" />
+    <a href="https://example.com/Meghana_Katraju_Resume.pdf" target="_blank">
+  <img src="/icons/resume-icon.svg" alt="Resume" />
       </a>
     </div>
   );
