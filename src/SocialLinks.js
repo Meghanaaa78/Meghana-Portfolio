a10571c71ca0134f52c959a895c27f7632a629e8
kMeghana <159526348+Meghanaaa78@users.noreply.github.com>
2025-03-21 10:48:53 -0500

    Update SocialLinks.js

--- src/SocialLinks.js
+++ src/SocialLinks.js
@@ -3,13 +3,13 @@ import React from 'react';
 const SocialLinks = () => {
   return (
     <div className="social-links">
-      <a href="https://github.com/MeghanaKatraju31" target="_blank" rel="noopener noreferrer">
+      <a href="https://github.com/Meghanaaa78" target="_blank" rel="noopener noreferrer">
         <img src="/icons/github-icon.svg" alt="GitHub" />
       </a>
-      <a href="https://www.linkedin.com/in/meghana-k-568934211/" target="_blank" rel="noopener noreferrer">
+      <a href="https://www.linkedin.com/in/meghtech/" target="_blank" rel="noopener noreferrer">
         <img src="/icons/linkedin-icon.svg" alt="LinkedIn" />
       </a>
-      <a href="https://yourwebsite.com/Meghana_Katraju_Resume.pdf" target="_blank" rel="noreferrer">
+      <a href="https://in.docworkspace.com/d/sIC7g9_2JAb-U9r4G" target="_blank" rel="noreferrer">
         <img src="/icons/resume-icon.svg" alt="Resume" />
       </a>
     </div>
